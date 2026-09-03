8e7a25d5abbb3467eadb57324cdec098baff6946
bernardoferreira154-sketch <bernardo_ferreira154@estudante.sesisenai.org.br>
2026-09-03 19:50:49 -0300

    Logica completa

--- script.js
+++ script.js
@@ -82,11 +82,7 @@ function ordenarPopulacao(populacao){
 }
 
 
-let populacao = criarPopulacao(100);
 
-let avaliados = avaliandoPopulacao(populacao);
-//teste
-console.log(ordenarPopulacao(avaliados))
 
 //selecionando os goat
 function selecionandoMelhores(populacao, quantidade){
@@ -94,10 +90,8 @@ function selecionandoMelhores(populacao, quantidade){
     return populacao.slice(0, quantidade);
 }
 
-let pais = selecionandoMelhores(avaliados, 10)
 
-//testes
-console.log(pais)
+
 
 
 //coito :/
@@ -119,4 +113,57 @@ return filho
 
 
 //TESTE
-console.log(coito("HEPPE", "OLLLO"));
+//console.log(coito("HEPPE", "OLLLO"));
+
+//transformando os caba em Xman (mutação)
+function mutar(individuo, taxa){
+    let resultado = "";
+
+    for (let i = 0; i < individuo.length; i++)
+        if (Math.random() < taxa){
+            let indice = Math.floor(Math.random() * letras.length);
+
+            resultado += letras[indice]
+        }else{
+            resultado += individuo[i]
+        }
+
+        return resultado
+}
+//teste
+console.log(mutar("HELLO", 0.25));
+
+//variaveis, valores, etc
+const tamanhoPopulacao = 100;
+const quantidadePais = 10;
+const taxaDeMutacao = 0.5;
+
+let populacao = criarPopulacao(tamanhoPopulacao);
+
+while (true) {
+
+    let avaliados = avaliandoPopulacao(populacao);
+    ordenarPopulacao(avaliados)
+
+        if (avaliados[0].fitness == objetivo.length){
+            console.log("ACHEI, SEU BABÃO. a palavra era", avaliados[0].individuo);
+            break;
+        }
+
+        let pais = selecionandoMelhores(avaliados, quantidadePais);
+        let novaPopulacao = []
+
+        while (novaPopulacao.length < tamanhoPopulacao){
+            let escolhePai1 = Math.floor(Math.random() * pais.length);
+            let pai1 = pais[escolhePai1];
+            let escolhePai2 = Math.floor(Math.random() * pais.length);
+            let pai2 = pais[escolhePai2]
+            let filho = coito(pai1.individuo, pai2.individuo)
+            filho = mutar(filho, taxaDeMutacao);
+            novaPopulacao.push(filho);
+        }
+        
+        populacao = novaPopulacao;
+
+    
+}
